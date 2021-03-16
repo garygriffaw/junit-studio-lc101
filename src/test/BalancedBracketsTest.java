69359310dc92bade700833a0d6e3b813a595ff56
garygriffaw <ggriffaw@yahoo.com>
2021-03-16 14:04:37 -0500

    Completed 6.4. Studio: Unit Testing

--- src/test/BalancedBracketsTest.java
+++ src/test/BalancedBracketsTest.java
@@ -1,5 +1,6 @@
 package test;
 
+import main.BalancedBrackets;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -8,9 +9,67 @@ public class BalancedBracketsTest {
 
     //TODO: add tests here
     @Test
-    public void emptyTest() {
-        assertEquals(true, true);
+    public void emptyReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets(""));
     }
 
+    @Test
+    public void noBracketsReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("abc"));
+    }
 
+    @Test
+    public void onlyBracketsReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[]"));
+    }
+
+    @Test
+    public void bracketsWithTextReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[a]"));
+    }
+
+    @Test
+    public void nestedBracketsWithTextReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[a[b]]"));
+    }
+
+    @Test
+    public void twoSameLevelNestedBracketsWithTextReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[a[b]cd[]]"));
+    }
+
+    @Test
+    public void twoLevelsDeepNestedBracketsWithTextReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[a[bc[d]]]"));
+    }
+
+    @Test
+    public void onlyLeftBracketReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("["));
+    }
+
+    @Test
+    public void onlyRightBracketReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("]"));
+    }
+
+    @Test
+    public void unbalancedRightBracketReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[]]"));
+    }
+
+    @Test
+    public void reversedBracketReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("]["));
+    }
+
+    @Test
+    public void unbalancedNestedBracketsWithTextReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[a[b]"));
+    }
+
+    @Test
+    public void unbalancedTwoLevelsDeepNestedBracketsWithTextReturnsTrue() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[a[bc][d]]]"));
+    }
 }
